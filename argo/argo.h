2c3686d9542b8a55854e4a6afe45d3521fc555db
Tony DiPasquale <tony@thoughtbot.com>
2014-09-22 19:42:14 -0400

    Initial commit

---
+++ argo/argo.h
@@ -0,0 +1,19 @@
+//
+//  argo.h
+//  argo
+//
+//  Created by Tony DiPasquale on 9/22/14.
+//  Copyright (c) 2014 thoughtbot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for argo.
+FOUNDATION_EXPORT double argoVersionNumber;
+
+//! Project version string for argo.
+FOUNDATION_EXPORT const unsigned char argoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <argo/PublicHeader.h>
+
+
