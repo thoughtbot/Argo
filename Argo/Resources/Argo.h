0dea798b4ba0074ba30b7fa2bf2ef545c003df40
Gordon Fontenot <gordon@thoughtbot.com>
2014-12-25 20:13:38 -0500

    Add OS X target
    Rearranged the project a little bit and added an OS X target so that
    this can be used for mac apps.

---
+++ Argo/Resources/Argo.h
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for Argo.
+FOUNDATION_EXPORT double ArgoVersionNumber;
+
+//! Project version string for Argo.
+FOUNDATION_EXPORT const unsigned char ArgoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Argo/PublicHeader.h>
