d99eaa3e02585b937b97c845d590cba53a43ebb0
Gordon Fontenot <gordon@thoughtbot.com>
2015-03-20 10:05:24 -0400

    Move files around to make more sense

--- Argo/Operators/Operators.swift
+++ Argo/Operators/Operators.swift
@@ -1,4 +1,51 @@
+import Runes
+
 infix operator <| { associativity left precedence 150 }
 infix operator <|? { associativity left precedence 150 }
 infix operator <|| { associativity left precedence 150 }
 infix operator <||? { associativity left precedence 150 }
+
+// MARK: Values
+
+// Pull value from JSON
+public func <|<A where A: JSONDecodable, A == A.DecodedType>(json: JSON, key: String) -> Decoded<A> {
+  return decodeObject(json) >>- { (A.decode <^> $0[key]) ?? .MissingKey(key) }
+}
+
+// Pull optional value from JSON
+public func <|?<A where A: JSONDecodable, A == A.DecodedType>(json: JSON, key: String) -> Decoded<A?> {
+  return .optional(json <| key)
+}
+
+// Pull embedded value from JSON
+public func <|<A where A: JSONDecodable, A == A.DecodedType>(json: JSON, keys: [String]) -> Decoded<A> {
+  return flatReduce(keys, json, <|) >>- A.decode
+}
+
+// Pull embedded optional value from JSON
+public func <|?<A where A: JSONDecodable, A == A.DecodedType>(json: JSON, keys: [String]) -> Decoded<A?> {
+  return .optional(json <| keys)
+}
+
+// MARK: Arrays
+
+// Pull array from JSON
+public func <||<A where A: JSONDecodable, A == A.DecodedType>(json: JSON, key: String) -> Decoded<[A]> {
+  return json <| key >>- decodeArray
+}
+
+// Pull optional array from JSON
+public func <||?<A where A: JSONDecodable, A == A.DecodedType>(json: JSON, key: String) -> Decoded<[A]?> {
+  return .optional(json <|| key)
+}
+
+// Pull embedded array from JSON
+public func <||<A where A: JSONDecodable, A == A.DecodedType>(json: JSON, keys: [String]) -> Decoded<[A]> {
+  return json <| keys >>- decodeArray
+}
+
+// Pull embedded optional array from JSON
+public func <||?<A where A: JSONDecodable, A == A.DecodedType>(json: JSON, keys: [String]) -> Decoded<[A]?> {
+  return .optional(json <|| keys)
+}
+
