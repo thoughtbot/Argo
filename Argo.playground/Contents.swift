15e91707e3b25d17054c225b109f13c017975602
Gordon Fontenot <gordon@thoughtbot.com>
2015-05-08 11:26:39 -0400

    Add playground
    Now that we're using git submodules (grumble grumble) and a workspace,
    we can add a playground to the workspace to act as a scratch
    pad/educational document for users.

---
+++ Argo.playground/Contents.swift
@@ -0,0 +1,21 @@
+import Argo
+import Runes
+
+struct User {
+    let id: Int
+    let name: String
+    let email: String?
+}
+
+extension User: Decodable  {
+    static func create(id: Int)(name: String)(email: String?) -> User {
+        return User(id: id, name: name, email: email)
+    }
+
+    static func decode(j: JSON) -> Decoded<User> {
+        return create
+            <^> j <| "id"
+            <*> j <| "name"
+            <*> j <|? "email"
+    }
+}
