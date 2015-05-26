82696d9de5c3221b2a51cbf59b8979c41d4eabe1
al_skipp <al_skipp@fastmail.fm>
2015-05-26 16:00:39 +0100

    Remove `return` from single-expression closure

--- Argo/Functions/sequence.swift
+++ Argo/Functions/sequence.swift
@@ -2,12 +2,12 @@ import Runes
 
 func sequence<T>(xs: [Decoded<T>]) -> Decoded<[T]> {
   return reduce(xs, pure([])) { accum, elem in
-    return curry(+) <^> accum <*> (pure <^> elem)
+    curry(+) <^> accum <*> (pure <^> elem)
   }
 }
 
 func sequence<T>(xs: [String: Decoded<T>]) -> Decoded<[String: T]> {
   return reduce(xs, pure([:])) { accum, elem in
-    return curry(+) <^> accum <*> ({ [elem.0: $0] } <^> elem.1)
+    curry(+) <^> accum <*> ({ [elem.0: $0] } <^> elem.1)
   }
 }
