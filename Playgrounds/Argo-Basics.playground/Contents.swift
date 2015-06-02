9d55345286f44c88a887083e18cef65627934fd6
al_skipp <al_skipp@fastmail.fm>
2015-06-02 20:46:43 +0100

    Update Playground examples
    This updates the current playground example to show the full path to
    decoding a `User` from JSON. It also adds a more complex example of
    decoding JSON from iTunes to get information about apps in the App
    Store.

---
+++ Playgrounds/Argo-Basics.playground/Contents.swift
@@ -0,0 +1,53 @@
+/*:
+**Note:** For **Argo** to be imported into the Playground, ensure that the **Argo-Mac** *scheme* is selected from the list of schemes.
+
+* * *
+*/
+import Foundation
+import Argo
+import Runes
+/*:
+**Helper function** – load JSON from a file
+*/
+func JSONFromFile(file: String) -> AnyObject? {
+  return NSBundle.mainBundle().pathForResource(file, ofType: "json")
+    >>- { NSData(contentsOfFile: $0) }
+    >>- { NSJSONSerialization.JSONObjectWithData($0, options: nil, error: nil) }
+}
+/*:
+## Decoding JSON into a simple **User** struct
+
+The **User** struct has three properties, one of which is an Optional value.
+
+(The example JSON file can be found in the **Resources** folder.)
+*/
+struct User {
+  let id: Int
+  let name: String
+  let email: String?
+}
+
+extension User: Printable {
+  var description: String {
+    return "name: \(name), id: \(id), email: \(email)"
+  }
+}
+
+extension User: Decodable  {
+  static func create(id: Int)(name: String)(email: String?) -> User {
+    return User(id: id, name: name, email: email)
+  }
+  
+  static func decode(j: JSON) -> Decoded<User> {
+    return create
+      <^> j <| "id"
+      <*> j <| "name"
+      <*> j <|? "email"
+  }
+}
+/*:
+* * *
+*/
+let user: User? = JSONFromFile("user_with_email") >>- decode
+println(user!)
+
