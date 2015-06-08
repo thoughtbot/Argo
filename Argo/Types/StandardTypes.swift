b1d1c0b96ff5f642764c47c91aa9049b941878cf
Tony DiPasquale <tony@thoughtbot.com>
2015-06-08 16:09:41 -0700

    Ignore function parameter names

--- Argo/Types/StandardTypes.swift
+++ Argo/Types/StandardTypes.swift
@@ -5,7 +5,7 @@ extension String: Decodable {
   public static func decode(j: JSON) -> Decoded<String> {
     switch j {
     case let .String(s): return pure(s)
-    default: return typeMismatch("String", object: j)
+    default: return typeMismatch("String", j)
     }
   }
 }
@@ -14,7 +14,7 @@ extension Int: Decodable {
   public static func decode(j: JSON) -> Decoded<Int> {
     switch j {
     case let .Number(n): return pure(n as Int)
-    default: return typeMismatch("Int", object: j)
+    default: return typeMismatch("Int", j)
     }
   }
 }
@@ -23,7 +23,7 @@ extension Int64: Decodable {
   public static func decode(j: JSON) -> Decoded<Int64> {
     switch j {
     case let .Number(n): return pure(n.longLongValue)
-    default: return typeMismatch("Int64", object: j)
+    default: return typeMismatch("Int64", j)
     }
   }
 }
@@ -32,7 +32,7 @@ extension Double: Decodable {
   public static func decode(j: JSON) -> Decoded<Double> {
     switch j {
     case let .Number(n): return pure(n as Double)
-    default: return typeMismatch("Double", object: j)
+    default: return typeMismatch("Double", j)
     }
   }
 }
@@ -41,7 +41,7 @@ extension Bool: Decodable {
   public static func decode(j: JSON) -> Decoded<Bool> {
     switch j {
     case let .Number(n): return pure(n as Bool)
-    default: return typeMismatch("Bool", object: j)
+    default: return typeMismatch("Bool", j)
     }
   }
 }
@@ -50,7 +50,7 @@ extension Float: Decodable {
   public static func decode(j: JSON) -> Decoded<Float> {
     switch j {
     case let .Number(n): return pure(n as Float)
-    default: return typeMismatch("Float", object: j)
+    default: return typeMismatch("Float", j)
     }
   }
 }
@@ -58,25 +58,25 @@ extension Float: Decodable {
 public func decodeArray<A where A: Decodable, A == A.DecodedType>(value: JSON) -> Decoded<[A]> {
   switch value {
   case let .Array(a): return sequence(A.decode <^> a)
-  default: return typeMismatch("Array", object: value)
+  default: return typeMismatch("Array", value)
   }
 }
 
 public func decodeObject<A where A: Decodable, A == A.DecodedType>(value: JSON) -> Decoded<[String: A]> {
   switch value {
   case let .Object(o): return sequence(A.decode <^> o)
-  default: return typeMismatch("Object", object: value)
+  default: return typeMismatch("Object", value)
   }
 }
 
-func decodedJSONForKey(json: JSON, key: String) -> Decoded<JSON> {
+func decodedJSONForKey(json: JSON, _ key: String) -> Decoded<JSON> {
   switch json {
   case let .Object(o): return guardNull(key, j: o[key] ?? .Null)
-  default: return typeMismatch("Object", object: json)
+  default: return typeMismatch("Object", json)
   }
 }
 
-private func typeMismatch<T>(expectedType: String, object: JSON) -> Decoded<T> {
+private func typeMismatch<T>(expectedType: String, _ object: JSON) -> Decoded<T> {
   return .TypeMismatch("\(object) is not a \(expectedType)")
 }
 
