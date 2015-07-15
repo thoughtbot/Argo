8e986cdcb95707da3ac364509ed7e131ed2a4be5
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2015-07-15 17:07:32 -0400

    Remove internal dependency on Runes.framework
    We don't actually need to use Runes internally, since we're mainly using
    Decoded as an applicative, and not Optional or Array. Everything that we
    were using Runes for can be implemented with instance methods or as a
    simple function (in the case of pure for Array)

--- Argo/Functions/flatReduce.swift
+++ Argo/Functions/flatReduce.swift
@@ -1,5 +1,3 @@
-import Runes
-
 func flatReduce<S: SequenceType, U>(sequence: S, initial: U, combine: (U, S.Generator.Element) -> Decoded<U>) -> Decoded<U> {
   return sequence.reduce(pure(initial)) { accum, x in
     accum >>- { combine($0, x) }
