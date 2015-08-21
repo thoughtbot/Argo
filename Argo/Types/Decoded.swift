1b82ace62c9025dcc9af99021f7cf31d8485c996
jdhealy <jdhealy@gmail.com>
2015-08-20 20:25:30 -0400

    Document monadic operators for Decoded.

--- Argo/Types/Decoded.swift
+++ Argo/Types/Decoded.swift
@@ -70,14 +70,46 @@ public func pure<A>(a: A) -> Decoded<A> {
 
 // MARK: Monadic Operators
 
+/**
+  flatMap a function over a `Decoded` value (right associative)
+  
+  - If the value is a failing case (`.TypeMismatch`, `.MissingKey`), the function will not be evaluated and this will return the value
+  - If the value is `.Success`, the function will be applied to the unwrapped value
+
+  - parameter a: A value of type `Decoded<A>`
+  - parameter f: A transformation function from type `A` to type `Decoded<B>`
+
+  - returns: A value of type `Decoded<U>`
+*/
 public func >>- <A, B>(a: Decoded<A>, f: A -> Decoded<B>) -> Decoded<B> {
   return a.flatMap(f)
 }
 
+/**
+  map a function over a `Decoded` value
+
+  - If the value is is a failing case (`.TypeMismatch`, `.MissingKey`), the function will not be evaluated and this will return the value
+  - If the value is `.Success`, the function will be applied to the unwrapped value
+
+  - parameter f: A transformation function from type `A` to type `B`
+  - parameter a: A value of type `Decoded<A>`
+
+  - returns: A value of type `Decoded<B>`
+*/
 public func <^> <A, B>(f: A -> B, a: Decoded<A>) -> Decoded<B> {
   return a.map(f)
 }
 
+/**
+  apply a `Decoded` function to a `Decoded` value
+  - If the function is a failing case (`.TypeMismatch`, `.MissingKey`), this will return the function's value
+  - If the function is a `.Success` case and the value is a failure case, this will return the value
+  - If both self and the function are `.Success`, the function will be applied to the unwrapped value
+
+  - parameter f: A `Decoded` transformation function from type `A` to type `B`
+
+  - returns: A value of type `Decoded<B>`
+*/
 public func <*> <A, B>(f: Decoded<A -> B>, a: Decoded<A>) -> Decoded<B> {
   return a.apply(f)
 }
