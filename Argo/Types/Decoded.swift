283162e61238294a37c6d146e10928de7e0d374a
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2015-09-04 15:21:30 -0400

    Convert Decoded into a more Result-like type
    This ditches the strategy of using multiple error states, for using a
    single error state with multiple possible error causes.
    
    The end result here is a simpler Decoded type, without sacrificing the
    flexibility and expressiveness that we'd run into by switching to Result
    itself. Additionally, this will make transforming from Decoded to Result
    completely trivial:
    
    ```swift
    func makeResult<T>(d: Deocded<T>) -> Result<T, DecodeError> {
      switch d {
      case let .Success(value): return .Success(value)
      case let .Failure(error): return .Failure(error)
      }
    }
    ```

--- Argo/Types/Decoded.swift
+++ Argo/Types/Decoded.swift
@@ -1,7 +1,20 @@
-public enum Decoded<T> {
-  case Success(T)
+public enum DecodeError: ErrorType {
   case TypeMismatch(String)
   case MissingKey(String)
+}
+
+extension DecodeError: CustomStringConvertible {
+  public var description: String {
+    switch self {
+    case let .TypeMismatch(s): return "TypeMismatch(\(s))"
+    case let .MissingKey(s): return "MissingKey(\(s))"
+    }
+  }
+}
+
+public enum Decoded<T> {
+  case Success(T)
+  case Failure(DecodeError)
 
   public var value: T? {
     switch self {
@@ -15,15 +28,15 @@ public extension Decoded {
   static func optional<T>(x: Decoded<T>) -> Decoded<T?> {
     switch x {
     case let .Success(value): return .Success(.Some(value))
-    case .MissingKey: return .Success(.None)
-    case let .TypeMismatch(string): return .TypeMismatch(string)
+    case .Failure(.MissingKey): return .Success(.None)
+    case let .Failure(.TypeMismatch(string)): return .Failure(.TypeMismatch(string))
     }
   }
 
   static func fromOptional<T>(x: T?) -> Decoded<T> {
     switch x {
     case let .Some(value): return .Success(value)
-    case .None: return .TypeMismatch("Expected .Some(\(T.self)), got .None")
+    case .None: return .Failure(.TypeMismatch("Expected .Some(\(T.self)), got .None"))
     }
   }
 }
@@ -31,9 +44,8 @@ public extension Decoded {
 extension Decoded: CustomStringConvertible {
   public var description: String {
     switch self {
-    case let .Success(x): return "Success(\(x))"
-    case let .TypeMismatch(s): return "TypeMismatch(\(s))"
-    case let .MissingKey(s): return "MissingKey(\(s))"
+    case let .Success(value): return "Success(\(value))"
+    case let .Failure(error): return "Failure(\(error))"
     }
   }
 }
@@ -42,24 +54,21 @@ public extension Decoded {
   func map<U>(f: T -> U) -> Decoded<U> {
     switch self {
     case let .Success(value): return .Success(f(value))
-    case let .MissingKey(string): return .MissingKey(string)
-    case let .TypeMismatch(string): return .TypeMismatch(string)
+    case let .Failure(error): return .Failure(error)
     }
   }
 
   func apply<U>(f: Decoded<T -> U>) -> Decoded<U> {
     switch f {
-    case let .Success(value): return value <^> self
-    case let .MissingKey(string): return .MissingKey(string)
-    case let .TypeMismatch(string): return .TypeMismatch(string)
+    case let .Success(function): return self.map(function)
+    case let .Failure(error): return .Failure(error)
     }
   }
 
   func flatMap<U>(f: T -> Decoded<U>) -> Decoded<U> {
     switch self {
     case let .Success(value): return f(value)
-    case let .MissingKey(string): return .MissingKey(string)
-    case let .TypeMismatch(string): return .TypeMismatch(string)
+    case let .Failure(error): return .Failure(error)
     }
   }
 }
--- ArgoTests/Models/NSURL.swift
+++ ArgoTests/Models/NSURL.swift
@@ -7,8 +7,8 @@ extension NSURL: Decodable {
   public class func decode(j: JSON) -> Decoded<NSURL> {
     switch j {
     case .String(let urlString):
-      return NSURL(string: urlString).map(pure) ?? .TypeMismatch("\(j) is not a URL")
-    default: return .TypeMismatch("\(j) is not a URL")
+      return NSURL(string: urlString).map(pure) ?? .Failure(.TypeMismatch("\(j) is not a URL"))
+    default: return .Failure(.TypeMismatch("\(j) is not a URL"))
     }
   }
 }
