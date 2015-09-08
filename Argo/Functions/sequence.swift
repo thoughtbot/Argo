1ffa3c1343d69ee5af2d45469e2cf3fc480bf1b6
Moritz Angermann <moritz.angermann@gmail.com>
2015-09-08 14:34:35 +0800

    Make sequence public

--- Argo/Functions/sequence.swift
+++ Argo/Functions/sequence.swift
@@ -1,10 +1,10 @@
-func sequence<T>(xs: [Decoded<T>]) -> Decoded<[T]> {
+public func sequence<T>(xs: [Decoded<T>]) -> Decoded<[T]> {
   return xs.reduce(pure([])) { accum, elem in
     curry(+) <^> accum <*> ({ [$0] } <^> elem)
   }
 }
 
-func sequence<T>(xs: [String: Decoded<T>]) -> Decoded<[String: T]> {
+public func sequence<T>(xs: [String: Decoded<T>]) -> Decoded<[String: T]> {
   return xs.reduce(pure([:])) { accum, elem in
     curry(+) <^> accum <*> ({ [elem.0: $0] } <^> elem.1)
   }
