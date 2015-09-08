09851c7f8b1dcc72645ed7aaaeaf419148b7b09a
Paul Young <paulyoungonline@gmail.com>
2015-09-08 15:20:48 -0700

    Work around memory leak with switch and as patterns.
    * https://github.com/thoughtbot/Argo/issues/205#issuecomment-138697781

--- Argo/Types/JSON.swift
+++ Argo/Types/JSON.swift
@@ -10,12 +10,16 @@ public enum JSON {
 
 public extension JSON {
   static func parse(json: AnyObject) -> JSON {
-    switch json {
-    case let v as [AnyObject]: return .Array(v.map(parse))
-    case let v as [Swift.String: AnyObject]: return .Object(v.map(parse))
-    case let v as Swift.String: return .String(v)
-    case let v as NSNumber: return .Number(v)
-    default: return .Null
+    if let v = json as? [AnyObject] {
+      return .Array(v.map(parse))
+    } else if let v = json as? [Swift.String: AnyObject] {
+      return .Object(v.map(parse))
+    } else if let v = json as? Swift.String {
+      return .String(v)
+    } else if let v = json as? NSNumber {
+      return .Number(v)
+    } else {
+      return .Null
     }
   }
 }
