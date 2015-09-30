78c6e10fd046a4fa4a546c78e884e3659857afe8
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2015-09-30 15:19:50 -0500

    Add `catDecoded` for dictionaries
    I said that I added this, but I actually didn't. Now I am.

--- Argo/Functions/catDecoded.swift
+++ Argo/Functions/catDecoded.swift
@@ -1,5 +1,11 @@
 public func catDecoded<T>(xs: [Decoded<T>]) -> [T] {
   return xs.reduce([]) { accum, elem in
-    return elem.map { accum + [$0] } ?? accum
+    elem.map { accum + [$0] } ?? accum
+  }
+}
+
+public func catDecoded<T>(xs: [String: Decoded<T>]) -> [String: T] {
+  return xs.reduce([:]) { accum, elem in
+    elem.1.map { accum + [elem.0: $0] } ?? accum
   }
 }
