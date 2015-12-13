c827ad01876ce57b660608cdba2213db0ed905c1
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2015-12-13 12:04:44 -0800

    Use `T` instead of `A` for generics in operators
    This is a holdover from when we thought Swift was going to use Haskell
    style generics (`A`, `B`, `C`, etc) instead of using named generic
    types. We still don't really have a good name for these types, but the
    swift community has essentially standardized on `T` for unnamed generic
    types, so we should use the same.

--- Argo/Operators/DecodeDecoded.swift
+++ Argo/Operators/DecodeDecoded.swift
@@ -1,43 +1,43 @@
 // MARK: Values
 
 // Pull value from JSON
-public func <| <A where A: Decodable, A == A.DecodedType>(json: JSON, key: String) -> Decoded<A> {
+public func <| <T where T: Decodable, T == T.DecodedType>(json: JSON, key: String) -> Decoded<T> {
   return json <| [key]
 }
 
 // Pull optional value from JSON
-public func <|? <A where A: Decodable, A == A.DecodedType>(json: JSON, key: String) -> Decoded<A?> {
+public func <|? <T where T: Decodable, T == T.DecodedType>(json: JSON, key: String) -> Decoded<T?> {
   return .optional(json <| [key])
 }
 
 // Pull embedded value from JSON
-public func <| <A where A: Decodable, A == A.DecodedType>(json: JSON, keys: [String]) -> Decoded<A> {
-  return flatReduce(keys, initial: json, combine: decodedJSON) >>- A.decode
+public func <| <T where T: Decodable, T == T.DecodedType>(json: JSON, keys: [String]) -> Decoded<T> {
+  return flatReduce(keys, initial: json, combine: decodedJSON) >>- T.decode
 }
 
 // Pull embedded optional value from JSON
-public func <|? <A where A: Decodable, A == A.DecodedType>(json: JSON, keys: [String]) -> Decoded<A?> {
+public func <|? <T where T: Decodable, T == T.DecodedType>(json: JSON, keys: [String]) -> Decoded<T?> {
   return .optional(json <| keys)
 }
 
 // MARK: Arrays
 
 // Pull array from JSON
-public func <|| <A where A: Decodable, A == A.DecodedType>(json: JSON, key: String) -> Decoded<[A]> {
+public func <|| <T where T: Decodable, T == T.DecodedType>(json: JSON, key: String) -> Decoded<[T]> {
   return json <|| [key]
 }
 
 // Pull optional array from JSON
-public func <||? <A where A: Decodable, A == A.DecodedType>(json: JSON, key: String) -> Decoded<[A]?> {
+public func <||? <T where T: Decodable, T == T.DecodedType>(json: JSON, key: String) -> Decoded<[T]?> {
   return .optional(json <|| [key])
 }
 
 // Pull embedded array from JSON
-public func <|| <A where A: Decodable, A == A.DecodedType>(json: JSON, keys: [String]) -> Decoded<[A]> {
-  return flatReduce(keys, initial: json, combine: decodedJSON) >>- Array<A>.decode
+public func <|| <T where T: Decodable, T == T.DecodedType>(json: JSON, keys: [String]) -> Decoded<[T]> {
+  return flatReduce(keys, initial: json, combine: decodedJSON) >>- Trray<T>.decode
 }
 
 // Pull embedded optional array from JSON
-public func <||? <A where A: Decodable, A == A.DecodedType>(json: JSON, keys: [String]) -> Decoded<[A]?> {
+public func <||? <T where T: Decodable, T == T.DecodedType>(json: JSON, keys: [String]) -> Decoded<[T]?> {
   return .optional(json <|| keys)
 }
