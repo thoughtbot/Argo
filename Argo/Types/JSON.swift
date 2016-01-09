11a6f3343317ef3f5e291f8fcd017d70a5115e38
Tony DiPasquale <tony@thoughtbot.com>
2016-01-08 17:04:26 -0800

    Use closures inside JSON cases for lazy decoding
    The use of closures in this manor increased the decoding time by 10x to
    1.1s. I don't think we should use this approach but it is interesting
    and wanted to hold onto it. We can alway drop the commit later.

--- Argo/Types/JSON.swift
+++ Argo/Types/JSON.swift
@@ -1,8 +1,8 @@
 import Foundation
 
 public enum JSON {
-  case Object([Swift.String: AnyObject])
-  case Array([AnyObject])
+  case Object(() -> [Swift.String: JSON])
+  case Array(() -> [JSON])
   case String(Swift.String)
   case Number(NSNumber)
   case Null
@@ -11,8 +11,8 @@ public enum JSON {
 public extension JSON {
   static func parse(json: AnyObject) -> JSON {
     switch json {
-    case let v as [AnyObject]: return .Array(v)
-    case let v as [Swift.String: AnyObject]: return .Object(v)
+    case let v as [AnyObject]: return .Array({ v.map(parse) })
+    case let v as [Swift.String: AnyObject]: return .Object({ v.map(parse) })
     case let v as Swift.String: return .String(v)
     case let v as NSNumber: return .Number(v)
     default: return .Null
@@ -31,8 +31,8 @@ extension JSON: CustomStringConvertible {
     switch self {
     case let .String(v): return "String(\(v))"
     case let .Number(v): return "Number(\(v))"
-    case let .Array(a): return "Array(\(a.description))"
-    case let .Object(o): return "Object(\(o.description))"
+    case let .Array(a): return "Array(\(a().description))"
+    case let .Object(o): return "Object(\(o().description))"
     case .Null: return "Null"
     }
   }
@@ -44,8 +44,8 @@ public func == (lhs: JSON, rhs: JSON) -> Bool {
   switch (lhs, rhs) {
   case let (.String(l), .String(r)): return l == r
   case let (.Number(l), .Number(r)): return l == r
-  case let (.Array(l), .Array(r)): return l.map(JSON.parse) == r.map(JSON.parse)
-  case let (.Object(l), .Object(r)): return l.map(JSON.parse) == r.map(JSON.parse)
+  case let (.Array(l), .Array(r)): return l() == r()
+  case let (.Object(l), .Object(r)): return l() == r()
   case (.Null, .Null): return true
   default: return false
   }
