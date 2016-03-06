3d4ab7126bd3015e17fee693a2fba9b5f0b7445e
Matt Diephouse <matt@diephouse.com>
2016-03-06 16:14:25 -0500

    Make DecodeError Hashable

--- Argo/Types/DecodeError.swift
+++ Argo/Types/DecodeError.swift
@@ -13,3 +13,32 @@ extension DecodeError: CustomStringConvertible {
     }
   }
 }
+
+extension DecodeError: Hashable {
+  public var hashValue: Int {
+    switch self {
+    case let .TypeMismatch(expected: expected, actual: actual):
+      return expected.hashValue ^ actual.hashValue
+    case let .MissingKey(string):
+      return string.hashValue
+    case let .Custom(string):
+      return string.hashValue
+    }
+  }
+}
+
+public func == (lhs: DecodeError, rhs: DecodeError) -> Bool {
+  switch (lhs, rhs) {
+  case let (.TypeMismatch(expected: expected1, actual: actual1), .TypeMismatch(expected: expected2, actual: actual2)):
+    return expected1 == expected2 && actual1 == actual2
+
+  case let (.MissingKey(string1), .MissingKey(string2)):
+    return string1 == string2
+
+  case let (.Custom(string1), .Custom(string2)):
+    return string1 == string2
+
+  default:
+    return false
+  }
+}
