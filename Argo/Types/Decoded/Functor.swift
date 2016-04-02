4f45510e58255ee7d6b1fb23833390f2801eeabb
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2016-04-02 12:41:54 -0500

    Update documentation for Decoded/Functor

--- Argo/Types/Decoded/Functor.swift
+++ Argo/Types/Decoded/Functor.swift
@@ -1,8 +1,10 @@
 /**
-  map a function over a `Decoded` value
+  Conditionally map a function over a `Decoded` value.
 
-  - If the value is is a failing case (`.TypeMismatch`, `.MissingKey`), the function will not be evaluated and this will return the value
-  - If the value is `.Success`, the function will be applied to the unwrapped value
+  - If the value is `.Failure`, the function will not be evaluated and this
+    will return `.Failure`.
+  - If the value is `.Success`, the function will be applied to the unwrapped
+    value.
 
   - parameter f: A transformation function from type `T` to type `U`
   - parameter x: A value of type `Decoded<T>`
@@ -14,6 +16,18 @@ public func <^> <T, U>(@noescape f: T -> U, x: Decoded<T>) -> Decoded<U> {
 }
 
 public extension Decoded {
+  /**
+    Conditionally map a function over `self`.
+
+    - If `self` is `.Failure`, the function will not be evaluated and this will
+      return `.Failure`.
+    - If `self` is `.Success`, the function will be applied to the unwrapped
+      value.
+
+    - parameter f: A transformation function from type `T` to type `U`
+
+    - returns: A value of type `Decoded<U>`
+  */
   func map<U>(@noescape f: T -> U) -> Decoded<U> {
     switch self {
     case let .Success(value): return .Success(f(value))
