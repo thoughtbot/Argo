b1adacab4f13fe6532c234d3d9880067d6dfc5a9
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2016-04-02 10:02:56 -0500

    Add inline documentation for `catDecoded`

--- Argo/Functions/catDecoded.swift
+++ Argo/Functions/catDecoded.swift
@@ -1,3 +1,18 @@
+/**
+  Create a new array of unwrapped `.Success` values, filtering out `.Failure`s.
+
+  This will iterate through the array of `Decoded<T>` elements and safely
+  unwrap the values.
+
+  If the element is `.Success(T)`, it will unwrap the value and add it into the
+  array.
+
+  If the element is `.Failure`, it will not be added to the new array.
+
+  - parameter xs: An array of `Decoded<T>` values
+
+  - returns: An array of unwrapped values of type `T`
+*/
 public func catDecoded<T>(xs: [Decoded<T>]) -> [T] {
   var accum: [T] = []
   accum.reserveCapacity(xs.count)
@@ -12,6 +27,22 @@ public func catDecoded<T>(xs: [Decoded<T>]) -> [T] {
   return accum
 }
 
+/**
+  Create a new dictionary of unwrapped `.Success` values, filtering out
+  `.Failure`s.
+
+  This will iterate through the dictionary of `Decoded<T>` elements and safely
+  unwrap the values.
+
+  If the element is `.Success(T)`, it will unwrap the value and assign it to
+  the existing key in the new dictionary.
+
+  If the element is `.Failure`, it will not be added to the new dictionary.
+
+  - parameter xs: A dictionary of `Decoded<T>` values assigned to `String` keys
+
+  - returns: A dictionary of unwrapped values of type `T` assigned to `String` keys
+*/
 public func catDecoded<T>(xs: [String: Decoded<T>]) -> [String: T] {
   var accum = Dictionary<String, T>(minimumCapacity: xs.count)
 
