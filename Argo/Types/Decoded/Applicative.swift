5f9312b4d34429243dbbc9b57f8c033e285f8d5b
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2016-04-02 12:53:36 -0500

    Update documentation for Decoded/Applicative

--- Argo/Types/Decoded/Applicative.swift
+++ Argo/Types/Decoded/Applicative.swift
@@ -1,12 +1,15 @@
 /**
-  apply a `Decoded` function to a `Decoded` value
+  Conditionally apply a `Decoded` function to a `Decoded` value.
 
-  - If the function is a failing case (`.TypeMismatch`, `.MissingKey`), this will return the function's value
-  - If the function is a `.Success` case and the value is a failure case, this will return the value
-  - If both self and the function are `.Success`, the function will be applied to the unwrapped value
+  - If either the function or value arguments are `.Failure`, this will return
+    `.Failure`. The function's `.Failure` takes precedence here, and will be
+    returned first. If the function is `.Success` and the value is `.Failure`,
+    then the value's `.Failure` will be returned.
+  - If both the function and value arguments are `.Success`, this will return
+    the result of the function applied to the unwrapped value.
 
   - parameter f: A `Decoded` transformation function from type `T` to type `U`
-  - parameter a: A value of type `Decoded<T>`
+  - parameter x: A value of type `Decoded<T>`
 
   - returns: A value of type `Decoded<U>`
 */
@@ -14,11 +17,33 @@ public func <*> <T, U>(f: Decoded<T -> U>, x: Decoded<T>) -> Decoded<U> {
   return x.apply(f)
 }
 
+/**
+  Wrap a value in the minimal context of `.Success`.
+
+  - parameter x: Any value
+
+  - returns: The provided value wrapped in `.Success`
+*/
 public func pure<T>(x: T) -> Decoded<T> {
   return .Success(x)
 }
 
 public extension Decoded {
+  /**
+    Conditionally apply a `Decoded` function to `self`.
+
+    - If either the function or `self` are `.Failure`, this will return
+      `.Failure`. The function's `.Failure` takes precedence here, and will be
+      returned first. If the function is `.Success` and `self` is `.Failure`,
+      then `self`'s `.Failure` will be returned.
+    - If both the function and `self` are `.Success`, this will return
+      the result of the function applied to the unwrapped value.
+
+    - parameter f: A `Decoded` transformation function from type `T` to type
+      `U`
+
+    - returns: A value of type `Decoded<U>`
+  */
   func apply<U>(f: Decoded<T -> U>) -> Decoded<U> {
     switch f {
     case let .Success(function): return self.map(function)
