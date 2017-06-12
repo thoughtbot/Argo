a77540aeaa530854c8c848cf431512a54dea21d5
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2017-06-12 14:19:46 -0500

    Use a file-based lookup method for fixtures
    When running our tests via `swift-test`, we can't use the bundle stuff
    to find out test fixtures. This means that we need to move to a
    file-based approach.
    
    The simplest solution is to use a relative path lookup based on where
    the filetype-specific loader lives. That way, we can look up local
    relative files quickly and easily.

--- Tests/ArgoTests/JSON/JSONFileReader.swift
+++ Tests/ArgoTests/JSON/JSONFileReader.swift
@@ -1,14 +1,17 @@
 import Foundation
 
+func path(forFile file: String, relativeTo relative: String = #file) -> String? {
+  return URL(string: relative)
+    .flatMap { URL(string: "\(file)", relativeTo: $0) }
+    .flatMap { $0.absoluteString }
+}
+
 func json(fromFile file: String) -> Any? {
-  return Bundle(for: JSONFileReader.self).path(forResource: file, ofType: "json")
-    .flatMap { URL(fileURLWithPath: $0) }
-    .flatMap { try? Data(contentsOf: $0) }
+  return path(forFile: "\(file).json")
+    .flatMap { FileManager.default.contents(atPath: $0) }
     .flatMap(JSONObjectWithData)
 }
 
 private func JSONObjectWithData(fromData data: Data) -> Any? {
   return try? JSONSerialization.jsonObject(with: data, options: [])
 }
-
-private class JSONFileReader { }
--- Tests/ArgoTests/plists/PListFileReader.swift
+++ Tests/ArgoTests/plists/PListFileReader.swift
@@ -2,12 +2,10 @@ import Foundation
 
 class PListFileReader {
   class func plist(fromFile file: String) -> Any? {
-    let path = Bundle(for: self).path(forResource: file, ofType: "plist")
+    let p = path(forFile: "\(file).plist")!
 
-    if let p = path {
-      if let dict = NSDictionary(contentsOfFile: p) { return dict }
-      if let arr = NSArray(contentsOfFile: p) { return arr }
-    }
+    if let dict = NSDictionary(contentsOfFile: p) { return dict }
+    if let arr = NSArray(contentsOfFile: p) { return arr }
 
     return .none
   }
