0f2b9ade698a458bd45e80be5ba2c560dc4acb8d
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2017-06-12 14:45:01 -0500

    Update Package.swift
    This updates our package file to be consistent with Swift 4 and SPM.
    This also allows us to now run `swift-test` for our test suite.

--- Package.swift
+++ Package.swift
@@ -1,8 +1,30 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
   name: "Argo",
+  products: [
+    .library(name: "Argo", targets: ["Argo"]),
+  ],
   dependencies: [
-    .Package(url: "https://github.com/thoughtbot/Runes.git", majorVersion: 4)
-  ]
+    .package(url: "https://github.com/thoughtbot/Runes.git", .branch("swift-4")),
+    .package(url: "https://github.com/thoughtbot/Curry.git", from: "3.0.0"),
+  ],
+  targets: [
+    .target(
+      name: "Argo",
+      dependencies: [
+        "Runes",
+      ]
+    ),
+    .testTarget(
+      name: "ArgoTests",
+      dependencies: [
+        "Argo",
+        "Curry",
+      ]
+    ),
+  ],
+  swiftLanguageVersions: [3, 4]
 )
